0469d5a091785e88dd07aa68cb46baaca4f81f02
OlhaBahno <olya.bagno@gmail.com>
2019-02-13 14:49:01 +0200

    add titles

--- main.rb
+++ main.rb
@@ -7,9 +7,10 @@ include WorkHash
 include Driver
 
 
-# @hash = { "a": 19, "b": 2, "c": 34, "d": 12, "e": 1, "f": 10 }
-
+@hash_1 = { "a": 19, "b": 2, "c": 34, "d": 12, "e": 1, "f": 10 }
+@title_1 = %w[Letter Digit]
 @hash = {}
+@title = %w[Name Time]
 
 launch('Firefox')
 @hash['Firefox'] = start_time('Firefox')
@@ -18,8 +19,11 @@ launch('Chrome')
 @hash['Chrome'] = start_time('Chrome')
 
 create_excel
-titles
-fill_table(@hash, @hash.values.min, @hash.values.max)
+titles(@title,1,1)
+fill_table(@hash, @hash.values.min, @hash.values.max, 2, 1)
+
+titles(@title_1, 1, 4)
+fill_table(@hash_1, @hash_1.values.min, @hash_1.values.max, 2, 4)
 
 
 
--- writer_excel.rb
+++ writer_excel.rb
@@ -10,15 +10,12 @@ module Excel
                                       border: :thin
   end
 
-  def titles
-    title = ["Name", "Time"]
+  def titles(title, row, col)
     format = Spreadsheet::Format.new size: 15,
                                      horizontal_align: :centre,
                                      pattern_fg_color: :orange,
                                      pattern: 1,
                                      border: :thin
-    row = 1
-    col = 1
     title.each do |item|
       write_value(row, col, item, format)
       col += 1
@@ -31,8 +28,7 @@ module Excel
     @sheet.row(row).set_format(col, form)
   end
 
-
-  def fill_table(hash_res,min,max)
+  def fill_table(hash_res,min,max,row,col)
     format_green = Spreadsheet::Format.new size: 12,
                                            horizontal_align: :centre,
                                            border: :thin,
@@ -43,18 +39,15 @@ module Excel
                                          border: :thin,
                                          pattern_fg_color: :red,
                                          pattern: 1
-    row = 2
-    col = 1
     hash_res.each do |k, v|
       write_value(row, col, k.to_s, @format)
-      col += 1
+
       case v
-      when min then write_value(row, col, v, format_green)
-      when max then write_value(row, col, v, format_red)
-      else write_value(row, 2, v, @format)
+      when min then write_value(row, col + 1, v, format_green)
+      when max then write_value(row, col + 1, v, format_red)
+      else write_value(row, col + 1, v, @format)
       end
       row += 1
-      col -= 1
     end
     @book.write "#{Dir.pwd}/new.xls"
   end
